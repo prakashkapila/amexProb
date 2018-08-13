bec58666bf8733d256620633aa61b95b98308ecb
kapila <kapila@bigdata>
2018-08-13 17:03:28 -0400

    Unit test changes

--- interview/src/main/java/com/amex/processors/PitchingProcessor.java
+++ interview/src/main/java/com/amex/processors/PitchingProcessor.java
@@ -25,8 +25,7 @@ public class PitchingProcessor implements Serializable {
 	 * Calculate the top 10 pitchers' average regular season and post-season ERAs
 	 * and average win/loss (w/(w+l)) percentages i.e. The top 10 pitcher's ERAs
 	 * ((0.65(player1) + 0.72(player2) + ...) / 10) and (win/loss of player1 +
-	 * win/loss of player2 + ...)/10 
-	 * The pitchers in the top 10 may not have been on
+	 * win/loss of player2 + ...)/10 The pitchers in the top 10 may not have been on
 	 * a team that made it to the post-season, so average the ERAs & win/loss of the
 	 * pitchers that made it into the post-season
 	 */
@@ -37,90 +36,81 @@ public class PitchingProcessor implements Serializable {
 	private static final long serialVersionUID = -1129645751543755808L;
 
 	public void startProcess(DbReader reader) throws IOException {
-		Dataset<Row> regular =getRegularSeasonPitching(reader,10); 
-  	 	Dataset<Row> post =getPostSeasonPitching(reader, 10);
- 	 	regular = regular.join(post,"playerID").groupBy("playerID","YearID","RegularavgERA","PostavgERA","RegularWinPercent","PostWinPercent","RegularLossPercent","PostLossPercent")
- 	 			//.agg(functions.avg("PostavgERA"), functions.sum("RegularWins"),functions.sum("PostWins"),functions.sum("RegularLosses"));
- 	 			.count();
- 	 			regular.show();
-	 	Dataset<Pitching> pitching = 
-	 			regular.map(new MapFunction<Row,Pitching>(){
-	 				private static final long serialVersionUID = 498803601214462167L;
- 					@Override
-					public Pitching call(Row value) throws Exception {
- 						Pitching pitch = new Pitching();
- 						pitch.apply(value);
- 						return pitch;
-					}}, Encoders.bean(Pitching.class));
-	 	pitching.show();
-	 	pitching.persist(StorageLevel.DISK_ONLY());
-	 	saveFile(pitching);
- 	}
-	private void saveFile(Dataset<Pitching> pitching,final FileWriter writer) throws IOException
-	{
- 		writer.write(Pitching.COLUMNS());
+		Dataset<Row> regular = getRegularSeasonPitching(reader, 10);
+		Dataset<Row> post = getPostSeasonPitching(reader, 10);
+		regular = regular.join(post, "playerID")
+				.groupBy("playerID", "YearID", "RegularavgERA", "PostavgERA", "RegularWinPercent", "PostWinPercent",
+						"RegularLossPercent", "PostLossPercent")
+				// .agg(functions.avg("PostavgERA"),
+				// functions.sum("RegularWins"),functions.sum("PostWins"),functions.sum("RegularLosses"));
+				.count();
+		regular.show();
+		Dataset<Pitching> pitching = regular.map(new MapFunction<Row, Pitching>() {
+			private static final long serialVersionUID = 498803601214462167L;
+
+			@Override
+			public Pitching call(Row value) throws Exception {
+				Pitching pitch = new Pitching();
+				pitch.apply(value);
+				return pitch;
+			}
+		}, Encoders.bean(Pitching.class));
+		pitching.show();
+		pitching.persist(StorageLevel.DISK_ONLY());
+		saveFile(pitching);
+	}
+
+	private void saveFile(Dataset<Pitching> pitching, final FileWriter writer) throws IOException {
+		writer.write(Pitching.COLUMNS());
 		writer.flush();
 		CSVWriter.writer = writer;
 		pitching.foreach(new CSVWriter());
 	}
-	
-	private void saveFile(Dataset<Pitching> pitching) throws IOException
-	{
-		File file = new File("Pitching"+System.currentTimeMillis()+".csv");
- 		file.createNewFile();
-		System.out.println("saving to file"+file);
+
+	private void saveFile(Dataset<Pitching> pitching) throws IOException {
+		File file = new File("Pitching" + System.currentTimeMillis() + ".csv");
+		file.createNewFile();
+		System.out.println("saving to file" + file);
 		FileWriter writer = new FileWriter(file);
-		saveFile(pitching,writer); 
-				
-	}
-private Dataset<Row> getRegularSeasonPitching(DbReader reader,int num) {
-	Dataset<Row> ret = getData(reader,num,"pitchingpost");//.
-	ret = ret.withColumnRenamed("totalWins", "RegularWins")
-			.withColumnRenamed("avgERA", "RegularavgERA")	
-			.withColumnRenamed("YearID", "RegularYearID")	
-			.withColumnRenamed("totalLoss", "RegularLosses")
-			.withColumnRenamed("WinPercent", "RegularWinPercent")
-			.withColumnRenamed("LossPercent", "RegularLossPercent");
-	
-		return 	ret;
- 	}
-	private Dataset<Row> getPostSeasonPitching(DbReader reader,int num) {
-			Dataset<Row> ret = getData(reader,num,"pitchingpost");//.
-			ret = ret.withColumnRenamed("totalWins", "PostWins")
-					.withColumnRenamed("avgERA", "PostavgERA")					
-					.withColumnRenamed("totalLoss", "PostLosses")
-					.withColumnRenamed("WinPercent", "PostWinPercent")
-					.withColumnRenamed("LossPercent", "PostLossPercent");
- 				return ret;
+		saveFile(pitching, writer);
+
 	}
 
-	private Dataset<Row> getData(DbReader reader,int num,String table)
-	{
+	private Dataset<Row> getRegularSeasonPitching(DbReader reader, int num) {
+		Dataset<Row> ret = getData(reader, num, "pitchingpost");// .
+		ret = ret.withColumnRenamed("totalWins", "RegularWins").withColumnRenamed("avgERA", "RegularavgERA")
+				.withColumnRenamed("YearID", "RegularYearID").withColumnRenamed("totalLoss", "RegularLosses")
+				.withColumnRenamed("WinPercent", "RegularWinPercent")
+				.withColumnRenamed("LossPercent", "RegularLossPercent");
+
+		return ret;
+	}
+
+	private Dataset<Row> getPostSeasonPitching(DbReader reader, int num) {
+		Dataset<Row> ret = getData(reader, num, "pitchingpost");// .
+		ret = ret.withColumnRenamed("totalWins", "PostWins").withColumnRenamed("avgERA", "PostavgERA")
+				.withColumnRenamed("totalLoss", "PostLosses").withColumnRenamed("WinPercent", "PostWinPercent")
+				.withColumnRenamed("LossPercent", "PostLossPercent");
+		return ret;
+	}
+
+	private Dataset<Row> getData(DbReader reader, int num, String table) {
 		Dataset<Row> regular = reader.read(table);
-		Dataset<Row> eraGroup = regular
-				.withColumn("ERAC", regular.col("ERA").cast(DataTypes.DoubleType))
-				.filter(functions.expr("ERAC > 0"))
-	 			.groupBy("ERAC","playerID")
-	 			.avg("ERAC")
-	 			//.withColumn("samp", functions.avg("ERA"))//.
-				.withColumnRenamed("avg(ERAC)", "avgERA")
-				.sort(new Column("avgERA"))
-				.limit(num);
-		
-		regular = regular.join(eraGroup,"playerID")
-				.groupBy("YearID","playerID","avgERA").agg(functions.sum("W"),functions.sum("L"))
-				.withColumnRenamed("sum(W)", "totalWins")
-				.withColumnRenamed("sum(L)", "totalLoss")
-				.withColumn("total", functions.expr("totalWins+totalLoss"))
+		Dataset<Row> eraGroup = regular.withColumn("ERAC", regular.col("ERA").cast(DataTypes.DoubleType))
+				.filter(functions.expr("ERAC > 0")).groupBy("ERAC", "playerID").avg("ERAC")
+				// .withColumn("samp", functions.avg("ERA"))//.
+				.withColumnRenamed("avg(ERAC)", "avgERA").sort(new Column("avgERA")).limit(num);
+
+		regular = regular.join(eraGroup, "playerID").groupBy("YearID", "playerID", "avgERA")
+				.agg(functions.sum("W"), functions.sum("L")).withColumnRenamed("sum(W)", "totalWins")
+				.withColumnRenamed("sum(L)", "totalLoss").withColumn("total", functions.expr("totalWins+totalLoss"))
 				.withColumn("WinPercent", functions.expr("totalWins/total"))
 				.withColumn("LossPercent", functions.expr("totalLoss/total"));
 		regular.show();
- 		return regular.na().drop();
+		return regular.na().drop();
 	}
-	
-	
-	public static void main(String arg[])
-	{
+
+	public static void main(String arg[]) {
 		Logger.getLogger("org.apache").setLevel(Level.ERROR);
 		PitchingProcessor proce = new PitchingProcessor();
 		try {
@@ -136,20 +126,19 @@ private Dataset<Row> getRegularSeasonPitching(DbReader reader,int num) {
 		try {
 			proce.startProcess(new DbReader());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 }
 
-class CSVWriter implements ForeachFunction<Pitching>
-{
-	
+class CSVWriter implements ForeachFunction<Pitching> {
+
 	private static final long serialVersionUID = 6319324370196471909L;
-	static  FileWriter writer = null;
+	static FileWriter writer = null;
+
 	@Override
 	public void call(Pitching t) throws Exception {
-		 writer.write(t != null ? t.toString():"null");
-		 writer.flush();
+		writer.write(t != null ? t.toString() : "null");
+		writer.flush();
 	}
-	}
+}
--- interview/src/main/java/com/amex/vo/Pitching.java
+++ interview/src/main/java/com/amex/vo/Pitching.java
@@ -1,81 +1,87 @@
 package com.amex.vo;
+
 import java.io.Serializable;
 import java.text.NumberFormat;
 
 import org.apache.spark.sql.Row;
 
-public class Pitching implements Serializable{
-	 
+public class Pitching implements Serializable {
+
 	private static final long serialVersionUID = 3615110930547267695L;
-	
+
 	private static final NumberFormat FORMAT = NumberFormat.getInstance();
 	Integer year;
-	String player; 
-	Double regularSeasonERA,postseasonERA,regularSeasonWinLoss,postSeasonWinLoss;
-	
+	String player;
+	Double regularSeasonERA, postseasonERA, regularSeasonWinLoss, postSeasonWinLoss;
+
 	public static String COLUMNS() {
 		return "Year, Player, Regular Season ERA, Regular Season Win/Loss, Post-season ERA, Post-season Win/Loss";
 	}
+
 	public void apply(Row value) {
-		// TODO Auto-generated method stub
-		//| playerID|YearID|RegularavgERA|PostavgERA|RegularWins|PostWins|RegularLosses|PostLosses
 		this.year = value.getInt(1);
-		this.regularSeasonWinLoss=value.getDouble(5);
-		this.postSeasonWinLoss= value.getDouble(6);
-		this.player = value.getString(0); 
-		regularSeasonERA= value.getDouble(2);
-		postseasonERA= value.getDouble(3);
+		this.regularSeasonWinLoss = value.getDouble(5);
+		this.postSeasonWinLoss = value.getDouble(6);
+		this.player = value.getString(0);
+		regularSeasonERA = value.getDouble(2);
+		postseasonERA = value.getDouble(3);
 	}
+
 	public String toString() {
-		StringBuilder sb= new StringBuilder();
-		sb.append(FORMAT.format(year)).append(",")
-		.append(player).append(",")
-		.append(FORMAT.format(regularSeasonERA)).append(",")
-		.append(FORMAT.format(regularSeasonWinLoss)).append(",")
-		.append(FORMAT.format(postseasonERA)).append(",")
-		.append(FORMAT.format(postSeasonWinLoss));
+		StringBuilder sb = new StringBuilder();
+		sb.append(FORMAT.format(year)).append(",").append(player).append(",").append(FORMAT.format(regularSeasonERA))
+				.append(",").append(FORMAT.format(regularSeasonWinLoss)).append(",")
+				.append(FORMAT.format(postseasonERA)).append(",").append(FORMAT.format(postSeasonWinLoss));
 		return sb.toString();
 	}
-	
-	//Mutator & Accessor methods
+
+	// Mutator & Accessor methods
 	public Integer getYear() {
 		return year;
 	}
+
 	public void setYear(Integer year) {
 		this.year = year;
 	}
+
 	public Double getRegularSeasonWinLoss() {
 		return regularSeasonWinLoss;
 	}
+
 	public void setRegularSeasonWinLoss(Double regularSeasonWinLoss) {
 		this.regularSeasonWinLoss = regularSeasonWinLoss;
 	}
+
 	public Double getPostSeasonWinLoss() {
 		return postSeasonWinLoss;
 	}
+
 	public void setPostSeasonWinLoss(Double postSeasonWinLoss) {
 		this.postSeasonWinLoss = postSeasonWinLoss;
 	}
+
 	public String getPlayer() {
 		return player;
 	}
+
 	public void setPlayer(String player) {
 		this.player = player;
 	}
+
 	public Double getRegularSeasonERA() {
 		return regularSeasonERA;
 	}
+
 	public void setRegularSeasonERA(Double regularSeasonERA) {
 		this.regularSeasonERA = regularSeasonERA;
 	}
+
 	public Double getPostseasonERA() {
 		return postseasonERA;
 	}
+
 	public void setPostseasonERA(Double postseasonERA) {
 		this.postseasonERA = postseasonERA;
 	}
 
-	
-
-	
 }
